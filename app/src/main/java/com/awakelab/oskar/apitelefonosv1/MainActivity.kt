1323556f17140b4f7c41bf20e983e511e23cd485
OskarUno <oorellana80@gmail.com>
2023-08-14 22:22:30 -0400

    Sprint Final Modulo 6. Se crea proyecto, agregan las dependencias.

---
+++ app/src/main/java/com/awakelab/oskar/apitelefonosv1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.awakelab.oskar.apitelefonosv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
